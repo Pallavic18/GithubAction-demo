db3ca70593cd51698e172d826b4d55d7e1ff6de0
Pallavic18 <50622007+Pallavic18@users.noreply.github.com>
2021-12-06 21:07:34 +0530

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,1 +1,26 @@
-echo "Hello from the test scrip"
+#!/bin/sh
+
+# Print usage information.
+function usage {
+
+    echo "sh ad_hoc_run.sh"
+    echo "-c <comma-separated-list-of-collection-names>"
+    echo "-e <env>"
+    echo "-d <notify-default-email-group>"
+    echo "-n <semi-colon-separated-list-of-email-addresses-to-notify>"
+    echo "-g <generate-lima-headers>"
+    echo "-l <lima-headers>"
+    echo "-s <smoke-tests>"
+    echo "-p <proxy-host>"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
